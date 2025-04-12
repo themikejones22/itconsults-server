53b0f28201bb61bfefb78f7263e42b77617da3f5
thewinpers <thewinpers@gmail.com>
2025-04-12 16:14:28 +0300

    added a console.log statement to debug errors

--- src/v1/services/system/payment/inner.js
+++ src/v1/services/system/payment/inner.js
@@ -27,27 +27,44 @@ module.exports.createPaymentSession = async (currency = "USD", amount) => {
     const sessionResponse = await axios.post(
       "https://api.sandbox.checkout.com/payment-sessions",
       {
-        amount: 1000,
+        amount: 6540,
         currency: "GBP",
-        reference: "ORD-123A",
-        display_name: "Online shop",
-        billing: {
+        reference: "get started guide",
+        shipping: {
           address: {
+            address_line1: "Checkout.com",
+            address_line2: "90 Tottenham Court Road",
+            city: "London",
+            state: "London",
+            zip: "W1T 4TJ",
             country: "GB",
           },
         },
-        customer: {
-          name: "Jia Tsang",
-          email: "jia.tsang@example.com",
+        billing: {
+          address: {
+            address_line1: "Checkout.com",
+            address_line2: "90 Tottenham Court Road",
+            city: "London",
+            state: "London",
+            zip: "W1T 4TJ",
+            country: "GB",
+          },
         },
+        "3ds": {
+          enabled: false,
+          attempt_n3d: false,
+          challenge_indicator: "no_preference",
+          exemption: "low_value",
+          allow_upgrade: true,
+        },
+        enabled_payment_methods: ["card"],
         success_url: "https://example.com/payments/success",
-        failure_url: "https://example.com/payments/failure",
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + process.env.CHECKOUT_SECRET_KEY, // replace with your real test secret key
+        failure_url: "https://example.com/payments/fail",
+        metadata: {
+          coupon_code: "20OFF",
+          partner_id: 123989,
         },
+        processing_channel_id: "pc_loilg3lb45ye5ehvxybkway3ku",
       }
     );
 
